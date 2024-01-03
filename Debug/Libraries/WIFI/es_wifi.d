efa3e166fbd10b88be5b0e6d6129ea27db65e315
roblamrod <roberlama@gmail.com>
2024-01-03 10:56:23 +0100

    First commit

---
+++ Debug/Libraries/WIFI/es_wifi.d
@@ -0,0 +1,5 @@
+Libraries/WIFI/es_wifi.o: ../Libraries/WIFI/es_wifi.c \
+ C:/Users/rober/STM32Cube/Repository/STM32Cube_FW_L4_V1.18.0/Projects/B-L475E-IOT01A/Applications/WiFi/Common/Inc/es_wifi.h \
+ C:/Users/rober/STM32Cube/Repository/STM32Cube_FW_L4_V1.18.0/Projects/B-L475E-IOT01A/Applications/WiFi/Common/Inc/es_wifi_conf.h
+C:/Users/rober/STM32Cube/Repository/STM32Cube_FW_L4_V1.18.0/Projects/B-L475E-IOT01A/Applications/WiFi/Common/Inc/es_wifi.h:
+C:/Users/rober/STM32Cube/Repository/STM32Cube_FW_L4_V1.18.0/Projects/B-L475E-IOT01A/Applications/WiFi/Common/Inc/es_wifi_conf.h:
---
+++ Debug/Libraries/coreMQTT/source/core_mqtt.d
@@ -0,0 +1,20 @@
+Libraries/coreMQTT/source/core_mqtt.o: \
+ ../Libraries/coreMQTT/source/core_mqtt.c \
+ C:/Users/rober/STM32CubeIDE/workspace_1.13.2/Trabajo_SCF_rpf/Libraries/coreMQTT/source/include/core_mqtt_config.h \
+ C:/Users/rober/STM32CubeIDE/workspace_1.13.2/Trabajo_SCF_rpf/Libraries/coreMQTT/source/include/core_mqtt_config.h \
+ ../Libraries/coreMQTT/source/include/core_mqtt.h \
+ ../Libraries/coreMQTT/source/include/core_mqtt_serializer.h \
+ ../Libraries/coreMQTT/source/include/core_mqtt_config.h \
+ ../Libraries/coreMQTT/source/include/../interface/transport_interface.h \
+ ../Libraries/coreMQTT/source/include/core_mqtt_state.h \
+ ../Libraries/coreMQTT/source/include/core_mqtt.h \
+ ../Libraries/coreMQTT/source/include/core_mqtt_default_logging.h
+C:/Users/rober/STM32CubeIDE/workspace_1.13.2/Trabajo_SCF_rpf/Libraries/coreMQTT/source/include/core_mqtt_config.h:
+C:/Users/rober/STM32CubeIDE/workspace_1.13.2/Trabajo_SCF_rpf/Libraries/coreMQTT/source/include/core_mqtt_config.h:
+../Libraries/coreMQTT/source/include/core_mqtt.h:
+../Libraries/coreMQTT/source/include/core_mqtt_serializer.h:
+../Libraries/coreMQTT/source/include/core_mqtt_config.h:
+../Libraries/coreMQTT/source/include/../interface/transport_interface.h:
+../Libraries/coreMQTT/source/include/core_mqtt_state.h:
+../Libraries/coreMQTT/source/include/core_mqtt.h:
+../Libraries/coreMQTT/source/include/core_mqtt_default_logging.h:
